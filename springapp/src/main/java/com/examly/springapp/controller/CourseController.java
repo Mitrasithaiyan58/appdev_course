7d21bfba3644648131968a66e34fa48b68212208
neojarvis <techadmin@iamneo.ai>
2025-08-02 12:08:12 +0000

    Manual-save-commit-02/08/2025--17:38:12

---
+++ springapp/src/main/java/com/examly/springapp/controller/CourseController.java
@@ -0,0 +1,33 @@
+package com.examly.springapp.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.examly.springapp.model.Course;
+import com.examly.springapp.service.CourseService;
+import java.util.List;
+@RestController
+@RequestMapping("/api/courses")
+@CrossOrigin(origins="http://localhost:3000")
+public class CourseController 
+{
+    
+@Autowired
+
+private CourseService courseService;
+@PostMapping
+public Course addCourse(@RequestBody Course course)
+{
+    return courseService.addCourse(course);
+}
+@GetMapping
+public List<Course>getAllCourses()
+{
+    return courseService.getAllCourses();
+}
+}
