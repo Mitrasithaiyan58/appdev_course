a491701ded8d0642ec60d9623919c7a556a747de
neojarvis <techadmin@iamneo.ai>
2025-08-02 04:52:01 +0000

    Manual-save-commit-02/08/2025--10:22:01

--- springapp/src/main/java/com/examly/springapp/model/Course.java
+++ springapp/src/main/java/com/examly/springapp/model/Course.java
@@ -10,8 +10,47 @@ public class Course{
     private String trainerName;
     private String courseDuration;
     private String startDate;
+    public courseDuration;
 }
-public class Course {
+public class Course ()
+{
+
+}
+public Course(String courseTitle,String trainerName,String courseDuration,String startDate)
+{
+    this.courseTitle=courseTitle;
+    this.trainerName=trainerName;
+    this.courseDuration=courseDuration;
+    this.startDate=startDate;
+}
+public long getId()
+{
+    return id;
+}
+public String getcourseTitle()
+{
+    return courseTitle;
+}
+public void setCourseTitle(String courseTitle)
+{
+    this.courseTitle=courseTitle;
+}
+public String getTrainerName()
+{
+    return trainerName;
+}
+public void setcourseDuration(String courseDuration)
+{
+    this.courseDuration=courseDuration;
+}
+public String getStartDate()
+{
+    return startDate;
+}
+public void setStartDate(String startDate)
+{
+    this.startDate=startDate;
+}
 
 }
 
