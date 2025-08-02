5d128d77ee8f83203d9b49d8667c778dae0b9bac
neojarvis <techadmin@iamneo.ai>
2025-08-02 11:55:16 +0000

    Manual-save-commit-02/08/2025--17:25:16

---
+++ springapp/src/main/java/com/examly/springapp/service/CourseService.java
@@ -0,0 +1,24 @@
+package com.examly.springapp.service;
+import com.examly.springapp.model.Course;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.examly.springapp.repository.CourseRepository;
+@Service
+public class CourseService {
+    @Autowired
+    private CourseRepository courseRepository;
+    public Course addCourse(Course course)
+    {
+        return courseRepository.save(course);
+    }
+    public List<Course> getAllCourses()
+    {
+        return courseRepository.findAll();
+    }
+
+}
+
+
