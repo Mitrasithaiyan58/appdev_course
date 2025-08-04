e9c4e34a18d379959abe18044a0a1dba9168ebd8
neojarvis <techadmin@iamneo.ai>
2025-08-04 15:50:00 +0000

    Manual-save-commit-04/08/2025--21:19:59

--- springapp/src/main/java/com/examly/springapp/controller/CourseController.java
+++ springapp/src/main/java/com/examly/springapp/controller/CourseController.java
@@ -1,6 +1,7 @@
 package com.examly.springapp.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.examly.springapp.model.Course;
 import com.examly.springapp.service.CourseService;
+
+import jakarta.validation.Valid;
+
 import java.util.List;
 @RestController
 @RequestMapping("/api/courses")
@@ -23,12 +27,28 @@ public class CourseController
 @Autowired
 
 private CourseService courseService;
+
+//post
 @PostMapping("/post")
-public Course addCourse(@RequestBody Course course)
+public ResponseEntity<Course> addCourse(@Valid @RequestBody Course course)
 {
     
-    return courseService.addCourse(course);
+    return ResponseEntity.ok (courseService.addCourse(course));
 }
+
+//get by id
+
+@GetMapping("get/{id}")
+public ResponseEntity<Course> getCourse(@PathVariable Long id)
+{
+    Course course=courseService.getCourseById(id);
+    if(course==null)
+    {
+        return ResponseEntity.notFound().build();
+    }
+    return ResponseEntity.ok(course);
+}
+
 @GetMapping("/get")
 public List<Course>getAllCourses()
 {
@@ -36,15 +56,16 @@ public List<Course>getAllCourses()
 }
 
 @PutMapping("/put/{id}")
-public Course updateCourse(@PathVariable Long id,@RequestBody Course course)
+public ResponseEntity<Course> updateCourse(@PathVariable Long id,@RequestBody Course course)
 {
-    return courseService.updateCourse(id,course);
+    Course updated=courseService.updateCourse(id,course);
+    return ResponseEntity.ok(updated);
 }
 
 @DeleteMapping("/delete/{id}")
-public String deleteCourse(@PathVariable Long id)
+public ResponseEntity<void> deleteCourse(@PathVariable Long id)
 {
     courseService.deleteCourse(id);
-    return "success";
+    return ResponseEntity.ok().build();
 }
 }
