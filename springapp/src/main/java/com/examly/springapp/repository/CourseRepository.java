db7a26055d0564cf84844439a5a26538cfb88431
neojarvis <techadmin@iamneo.ai>
2025-08-04 15:50:51 +0000

    Manual-save-commit-04/08/2025--21:20:50

--- springapp/src/main/java/com/examly/springapp/repository/CourseRepository.java
+++ springapp/src/main/java/com/examly/springapp/repository/CourseRepository.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface CourseRepository extends JpaRepository<Course,Long>{
+public interface CourseRepository extends JpaRepository<Course,Long>
+{
 
 }
