5df5d21a048d189471f3ed295f049e262b5e6308
neojarvis <techadmin@iamneo.ai>
2025-08-07 04:47:36 +0000

    Manual-save-commit-07/08/2025--10:17:36

---
+++ reactapp/src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, React!</h1>
+      <p>This is styled using index.css</p>
+    </div>
+  );
+}
+
+export default App; // ✅ default export
